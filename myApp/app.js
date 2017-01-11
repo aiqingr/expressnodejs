88e6d9cd5b5364a90efce8902081c25e7651e141
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-11 10:20:07 -0500

    delete the wrong myapp

--- myApp/app.js
+++ myApp/app.js
@@ -5,6 +5,18 @@ app.get('/', function (req, res) {
   res.send('Hello yoyo!')
 })
 
+app.post('/', function (req, res) {
+  res.send('Hello Chen!')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Hello Tian!')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Hello Qi')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on 3000')
 })
